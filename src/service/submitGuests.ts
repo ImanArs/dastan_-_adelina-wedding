1dc6e1850f4b359b054b9b26d75db0d7463fb014
ImanArs <imanbaevemaill.ru@gmail.com>
2024-12-16 17:16:58 +0600

    fix url backend

--- src/service/submitGuests.ts
+++ src/service/submitGuests.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const submitGuest = async (data: any) => {
   try {
-    const resp = await axios.post('http://13.60.73.151:8000/api/guests/', data)
+    const resp = await axios.post('https://13.60.73.151:8000/api/guests/', data)
     console.log(resp);
     return resp
   } catch (error) {
